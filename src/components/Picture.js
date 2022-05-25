abe5b02f041c68c40a61e45b68b1afc115ee9940
Oussama <guezguezoussama@gmail.com>
2022-05-26 01:42:15 +0200

    first-commit

---
+++ src/components/Picture.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Picture.css'
+function Picture() {
+  return (
+    <div className='up-container'>
+        <div className='img-profile'>
+            <img src='./benzema.jpg' alt='Profile Picture' height="100px" width="100px"/>
+        </div>
+    </div>
+  )
+}
+
+export default Picture
---
+++ src/components/ProfileDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./ProfileDetails.css"
+
+function ProfileDetails() {
+  return (
+    <div className='container'>
+        <h3>Karim Benzema</h3>
+        <h4>Madrid, Spain</h4>
+        <p>Karim Mostafa Benzema (born 19 December 1987) is a French professional footballer who plays as a striker for La Liga club Real Madrid and the France national team. A creative and prolific forward known for his playmaking and finishing, he is regarded as one of the best forwards of his generation</p>
+    </div>
+  )
+}
+
+export default ProfileDetails
